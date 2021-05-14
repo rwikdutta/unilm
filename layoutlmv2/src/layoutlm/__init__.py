fdfd1bd5e0ed93edc0c48e1c0193e734514466a2
rwikdutta <rwikdutta@outlook.com>
2021-05-14 14:46:50 +0000

    Supporting Sequence Classification for Layoutlmv2

--- layoutlmv2/src/layoutlm/__init__.py
+++ layoutlmv2/src/layoutlm/__init__.py
@@ -1,4 +1,4 @@
-from transformers import CONFIG_MAPPING, MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING, MODEL_NAMES_MAPPING, TOKENIZER_MAPPING
+from transformers import CONFIG_MAPPING, MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING, MODEL_NAMES_MAPPING, TOKENIZER_MAPPING, MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING
 from transformers.convert_slow_tokenizer import SLOW_TO_FAST_CONVERTERS, BertConverter
 from transformers.models.auto.modeling_auto import auto_class_factory
 
@@ -7,6 +7,7 @@ from .modeling.layoutlmv2 import (
     LayoutLMv2ForTokenClassification,
     LayoutLMv2Tokenizer,
     LayoutLMv2TokenizerFast,
+    LayoutLMv2ForSequenceClassification
 )
 
 
@@ -17,4 +18,8 @@ SLOW_TO_FAST_CONVERTERS.update({"LayoutLMv2Tokenizer": BertConverter})
 MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING.update([(LayoutLMv2Config, LayoutLMv2ForTokenClassification)])
 AutoModelForTokenClassification = auto_class_factory(
     "AutoModelForTokenClassification", MODEL_FOR_TOKEN_CLASSIFICATION_MAPPING, head_doc="token classification"
+)
+MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING.update([LayoutLMv2Config,LayoutLMv2ForSequenceClassification])
+AutoModelForSequenceClassification = auto_class_factory(
+  "AutoModelForSequenceClassification",MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING,head_doc="sequence classification"
 )
--- layoutlmv2/src/layoutlm/modeling/layoutlmv2/modeling_layoutlmv2.py
+++ layoutlmv2/src/layoutlm/modeling/layoutlmv2/modeling_layoutlmv2.py
@@ -15,6 +15,7 @@ from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions,
     BaseModelOutputWithPoolingAndCrossAttentions,
     TokenClassifierOutput,
+    SequenceClassifierOutput
 )
 from transformers.modeling_utils import apply_chunking_to_forward, find_pruneable_heads_and_indices, prune_linear_layer
 from transformers.models.layoutlm.modeling_layoutlm import LayoutLMIntermediate as LayoutLMv2Intermediate
@@ -868,6 +869,76 @@ class LayoutLMv2ForTokenClassification(LayoutLMv2PreTrainedModel):
             return ((loss,) + output) if loss is not None else output
 
         return TokenClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+class LayoutLMv2ForSequenceClassification(LayoutLMv2PreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.layoutlmv2 = LayoutLMv2Model(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def get_input_embeddings(self):
+        return self.layoutlmv2.embeddings.word_embeddings
+
+    def forward(
+        self,
+        input_ids=None,
+        bbox=None,
+        image=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.layoutlmv2(
+            input_ids=input_ids,
+            bbox=bbox,
+            image=image,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        seq_length = input_ids.size(1)
+        pooled_output=outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
             loss=loss,
             logits=logits,
             hidden_states=outputs.hidden_states,
